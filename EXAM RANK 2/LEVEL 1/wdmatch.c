06ad17faf7c6e0a3be99d8aeba72353e0da404f5
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-07 15:19:26 +0200

    Add files via upload

--- EXAM RANK 2/LEVEL 1/wdmatch.c
+++ EXAM RANK 2/LEVEL 1/wdmatch.c
@@ -27,46 +27,38 @@ $
 
 ------------------------------------------*/
 
-#include <unistd.h>
+##include <unistd.h>
 
-int main(int ac, char **av) 
+int main(int ac, char **av)
 {
-    int len1 = 0;
-    int len2 = 0;
-    int i = 0;
-    int j = 0;
-
-    if (ac != 3) 
+    if (ac != 3)
     {
         write(1, "\n", 1);
         return 0;
     }
-    while (av[1][len1])
-        len1++;
-    while (av[2][len2])
-        len2++;
-    while (i < len1 && j < len2)
+
+    int i = 0;
+    int j = 0;
+    while (av[1][i])
     {
-        if (av[1][i] == av[2][j])
+        while (av[2][j] && av[1][i] != av[2][j])
+                j++;
+        if (av[2][j] == '\0')
         {
-		i++;
+            write(1, "\n", 1);
+            return 0;
         }
+        i++;
         j++;
     }
-    if (i == len1)
+
+    i = 0;
+    while (av[1][i])
     {
-        i = 0;
-        while (av[1][i])
-        {
-            write(1, &av[1][i], 1);
-            i++;
-        }
-        write(1, "\n", 1);
+        write(1, &av[1][i], 1);
+        i++;
     }
-    else 
-    {
-        write(1, "\n", 1);
-    }
+    write(1, "\n", 1);
     return 0;
 }
 
