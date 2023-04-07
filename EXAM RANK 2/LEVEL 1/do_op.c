df7ad42f188e9122df2395e48f9f3b24d99bf285
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-07 15:56:59 +0200

    Update do_op.c

--- EXAM RANK 2/LEVEL 1/do_op.c
+++ EXAM RANK 2/LEVEL 1/do_op.c
@@ -34,77 +34,58 @@ $
 
 int ft_atoi(const char *str)
 {
-	int	sign;
-	int	result;
-	int	i;
+    int sign = 1;
+    int result = 0;
+    int i = 0;
 
-	sign = 1;
-	result = 0;
-	i = 0;
-	if (str[0] == '-')
-	{
-		sign = -1;
-		i = 1;
-	}
-	while (str[i] != '\0')
-	{
-		result = result * 10 + (str[i] - '0');
-		i++;
-	}
-	return sign * result;
+    if (str[0] == '-')
+    {
+        sign = -1;
+        i = 1;
+    }
+    while (str[i] != '\0')
+    {
+        result = result * 10 + (str[i] - '0');
+        i++;
+    }
+    return sign * result;
 }
 
 void	ft_putnbr(int n) 
 {
-	char c;
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
-	}
-	else
-	{
-		c = n + '0';
-		write(1, &c, 1);
-	}
+    char c;
+    if (n < 0)
+    {
+        write(1, "-", 1);
+        n = -n;
+    }
+    if (n >= 10)
+        ft_putnbr(n / 10);
+    c = n % 10 + '0';
+    write(1, &c, 1);
 }
 
-int main(int argc, char **argv) 
+int main(int ac, char **av) 
 {
-    if (argc != 4) 
+    if (ac != 4) 
     {
         write(1, "\n", 1);
         return 0;
     }
-    int op1 = ft_atoi(argv[1]);
-    int op2 = ft_atoi(argv[3]);
-    char op = argv[2][0];
+    int op1 = ft_atoi(av[1]);
+    int op2 = ft_atoi(av[3]);
+    char op = av[2][0];
     int result;
-    if (op == '+') 
-    {
+    if (op == '+')
         result = op1 + op2;
-    }
-    else if (op == '-') 
-    {
+    else if (op == '-')
         result = op1 - op2;
-    }
     else if (op == '*') 
-    {
         result = op1 * op2;
-    } 
-    else if (op == '/') 
-    {
+    else if (op == '/')
         result = op1 / op2;
-    }
     else if (op == '%') 
-    {
         result = op1 % op2;
-    } 
     else
     {
         write(1, "\n", 1);
@@ -114,4 +95,5 @@ int main(int argc, char **argv)
     write(1, "\n", 1);
     return 0;
 }
+
 
