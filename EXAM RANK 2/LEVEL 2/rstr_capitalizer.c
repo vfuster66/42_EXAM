14d8d40eac69d17398aa51e9e9af208203fd6187
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-10 20:16:41 +0200

    Update rstr_capitalizer.c

--- EXAM RANK 2/LEVEL 2/rstr_capitalizer.c
+++ EXAM RANK 2/LEVEL 2/rstr_capitalizer.c
@@ -30,37 +30,35 @@ $>
 
 #include <unistd.h>
 
-void    rstr_capitalizer(char *str)
-{
-    int i = 0;
-
-    while (str[i])
-    {
-        if (str[i] >= 'A' && str[i] <= 'Z')
-            str[i] += 32;
-        if ((str[i] >= 'a' && str[i] <= 'z') && (str[i + 1] == ' ' \
-                    || str[i + 1] == '\t' || str[i + 1] == '\0'))
-            str[i] -= 32;
-        write(1, &str[i++], 1);
-    }
-}
-
 int main(int ac, char **av)
 {
-    int i;
+    int i, j;
 
     if (ac == 1)
-        write(1, "\n", 1);
-    else
     {
-        i = 1;
-        while (i < ac)
-        {
-            rstr_capitalizer(av[i]);
-            write(1, "\n", 1);
-            i += 1;
-        }
+        write(1, "\n", 1);
+        return (0);
     }
+
+    i = 1;
+    while (i < ac)
+    {
+        j = 0;
+        while (av[i][j])
+        {
+            if (av[i][j] >= 'A' && av[i][j] <= 'Z')
+                av[i][j] += 32;
+            if ((av[i][j] >= 'a' && av[i][j] <= 'z') && (av[i][j + 1] == ' ' \
+                        || av[i][j + 1] == '\t' || av[i][j + 1] == '\0'))
+                av[i][j] -= 32;
+            write(1, &av[i][j], 1);
+            j++;
+        }
+        write(1, "\n", 1);
+        i++;
+    }
+
     return (0);
 }
+
 
