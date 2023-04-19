12d20934b770ccf73d0cd929a01502bdb3388524
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-19 13:34:54 +0200

    Update expand_str.c

--- EXAM RANK 2/LEVEL 2/expand_str.c
+++ EXAM RANK 2/LEVEL 2/expand_str.c
@@ -39,31 +39,30 @@ $>
 
 #include <unistd.h>
 
-int is_space(char c)
+int		main(int argc, char const *argv[])
 {
-    return (c == ' ' || c == '\t');
+	int		i;
+	int		flg;
+
+	if (argc == 2)
+	{
+		i = 0;
+		while (argv[1][i] == ' ' || argv[1][i] == '\t')
+			i += 1;
+		while (argv[1][i])
+		{
+			if (argv[1][i] == ' ' || argv[1][i] == '\t')
+				flg = 1;
+			if (!(argv[1][i] == ' ' || argv[1][i] == '\t'))
+			{
+				if (flg)
+					write(1, "   ", 3);
+				flg = 0;
+				write(1, &argv[1][i], 1);
+			}
+			i += 1;
+		}
+	}
+	write(1, "\n", 1);
+	return (0);
 }
-
-int main(int ac, char **av)
-{
-    if (ac != 2)
-    {
-        write(1, "\n", 1);
-        return 0;
-    }
-    int i = 0;
-    while (is_space(av[1][i]))
-        i++;
-    while (av[1][i])
-    {
-        if (!is_space(av[1][i]))
-            write(1, &av[1][i], 1);
-        else if (!is_space(av[1][i + 1]) && av[1][i + 1])
-            write(1, "   ", 3);
-        i++;
-    }
-    write(1, "\n", 1);
-    return 0;
-}
-
-
