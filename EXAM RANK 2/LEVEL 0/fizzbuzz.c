d3ad3f1e80da17a1cec45be6c2156d68ad1e7e0d
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 07:41:27 +0200

    Update fizzbuzz.c

--- EXAM RANK 2/LEVEL 0/fizzbuzz.c
+++ EXAM RANK 2/LEVEL 0/fizzbuzz.c
@@ -42,32 +42,45 @@ $>
 
 void    ft_putnbr(int n)
 {
-        char c;
-
+        char    c;
+        
         if (n >= 10)
+                // Récursivement, on divise le nombre par 10 pour extraire chaque chiffre
                 ft_putnbr(n / 10);
+        // On ajoute '0' pour obtenir le caractère correspondant à ce chiffre dans la table ASCII
         c = n % 10 + '0';
+        // On écrit ce caractère sur la sortie standard
         write(1, &c, 1);
 }
 
-int             main(void)
+int     main(void)
 {
-        int             i;
-
+        int     i;
+        
         i = 1;
         while (i <= 100)
         {
+                // Si le nombre est divisible par 3 et 5
                 if (i % 3 == 0 && i % 5 == 0)
+                        // On écrit "fizzbuzz" sur la sortie standard
                         write(1, "fizzbuzz", 8);
+                // Si le nombre est divisible par 3
                 else if (i % 3 == 0)
+                        // On écrit "fizz" sur la sortie standard
                         write(1, "fizz", 4);
+                // Si le nombre est divisible par 5
                 else if (i % 5 == 0)
+                        // On écrit "buzz" sur la sortie standard
                         write(1, "buzz", 4);
                 else
+                        // Sinon, on écrit le nombre sur la sortie standard
                         ft_putnbr(i);
+                // On ajoute une nouvelle ligne à la fin de chaque sortie
                 write(1, "\n", 1);
+                // On incrémente i pour passer au nombre suivant
                 i++;
         }
         return (0);
 }
+
 
