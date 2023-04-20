3ce3779d88a9e9d75425e46b59c2120ee58b03e2
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 13:28:44 +0200

    Update ft_itoa.c

--- EXAM RANK 2/LEVEL 3/ft_itoa.c
+++ EXAM RANK 2/LEVEL 3/ft_itoa.c
@@ -11,44 +11,57 @@ Your function must be declared as follows:
 char	*ft_itoa(int nbr);
 
 ------------------------------*/
-
 #include <stdlib.h>
 
 char	*ft_itoa(int n)
 {
+    // pointeur vers le début de la chaîne de caractères
     char	*str;
+    // longueur de la chaîne de caractères
     int	len;
+    // indicateur pour savoir si le nombre est négatif
     int	sign;
+    // variable temporaire pour stocker la valeur de n
     int	temp;
 
+    // retourne la chaîne de caractères "0" si n est égal à 0
     if (n == 0)
-        return ("0");
+        return ("0");   
     len = 0;
     sign = 0;
     if (n < 0)
     {
+        // indique que le nombre est négatif
         sign = 1;
-        n = -n;
+        // change la valeur de n en positive
+        n = -n;         
     }
     temp = n;
     while (temp > 0)
     {
-        temp /= 10;
+        // divise temp par 10 pour obtenir le nombre de chiffres dans n
+        temp /= 10;     
         len++;
     }
     if (sign)
+        // ajoute 1 à la longueur de la chaîne de caractères si le nombre est négatif
         len++;
+    // alloue de la mémoire pour la chaîne de caractères
     str = (char *)malloc(sizeof(char) * (len + 1));
-    str[len] = '\0';
+    // ajoute un caractère nul à la fin de la chaîne de caractères
+    str[len] = '\0';    
     while (len > 0)
     {
         len--;
-        str[len] = (n % 10) + '0';
+        // ajoute le chiffre correspondant à la fin de la chaîne de caractères
+        str[len] = (n % 10) + '0';  
         n /= 10;
     }
     if (sign)
+        // ajoute le signe "-" au début de la chaîne de caractères si le nombre est négatif
         str[0] = '-';
-    return (str);
+    // retourne la chaîne de caractères représentant n
+    return (str);       
 }
 
 /*---------------------------------------
