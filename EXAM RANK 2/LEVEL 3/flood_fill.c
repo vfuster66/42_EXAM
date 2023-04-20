9a699450bfe79e1c4c54e3a5e387378cf1bbc718
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 12:37:58 +0200

    Update flood_fill.c

--- EXAM RANK 2/LEVEL 3/flood_fill.c
+++ EXAM RANK 2/LEVEL 3/flood_fill.c
@@ -89,30 +89,40 @@ $>
 #include <stdlib.h>
 #include <stdio.h>
 
+// Définition de la structure de données pour un point
 typedef struct s_point
 {
     int x;
     int y;
 } t_point;
 
+// Fonction récursive qui remplit une zone de caractères identiques à partir d'un point donné
 void fill_area(char **tab, t_point size, t_point begin, char c)
 {
+    // On vérifie si le point est en dehors des limites du tableau
     if (begin.x < 0 || begin.x >= size.x || begin.y < 0 || begin.y >= size.y)
         return;
+    
+    // On vérifie si le caractère du point actuel est différent de celui qu'on cherche à remplacer
     if (tab[begin.y][begin.x] != c)
         return;
+    
+    // On marque le point actuel comme étant rempli
     tab[begin.y][begin.x] = 'F';
+    
+    // On remplit les zones voisines récursivement
     fill_area(tab, size, (t_point){begin.x + 1, begin.y}, c);
     fill_area(tab, size, (t_point){begin.x - 1, begin.y}, c);
     fill_area(tab, size, (t_point){begin.x, begin.y + 1}, c);
     fill_area(tab, size, (t_point){begin.x, begin.y - 1}, c);
 }
 
+// Fonction principale qui appelle fill_area pour remplir la zone correspondante
 void flood_fill(char **tab, t_point size, t_point begin)
 {
+    // On récupère le caractère à remplacer
     char c = tab[begin.y][begin.x];
+    
+    // On appelle fill_area pour remplir la zone correspondante
     fill_area(tab, size, begin, c);
 }
-
-
-
