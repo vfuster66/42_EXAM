2870986f736ce10459f1e31f6a3d73ad0ede7256
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 07:56:39 +0200

    Update repeat_alpha.c

--- EXAM RANK 2/LEVEL 0/repeat_alpha.c
+++ EXAM RANK 2/LEVEL 0/repeat_alpha.c
@@ -39,16 +39,21 @@ int     main(int ac, char **av)
         int k;
     
         i = 0;
+        // Vérification que le programme est appelé avec un seul argument
         if (ac == 2)
         {
+                // Parcours de chaque caractère de l'argument
                 while (av[1][i])
                 {
                         k = 1;
+                        // Si le caractère est une lettre majuscule, on lui attribue un multiplicateur de 1 à 26
                         if (av[1][i] >= 'A' && av[1][i] <= 'Z')
                                 k = av[1][i] - 64;
+                        // Si le caractère est une lettre minuscule, on lui attribue un multiplicateur de 1 à 26
                         if (av[1][i] >= 'a' && av[1][i] <= 'z')
                                 k = av[1][i] - 96;
                         j = 0;
+                        // On écrit le caractère autant de fois que son multiplicateur
                         while (j < k)
                         {
                                 write(1, &av[1][i], 1);
@@ -57,8 +62,7 @@ int     main(int ac, char **av)
                         i++;
                 }
         }
+        // On écrit un retour à la ligne à la fin de l'exécution
         write(1, "\n", 1);
         return (0);
 }
-
-
