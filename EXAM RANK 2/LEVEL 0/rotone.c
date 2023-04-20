ea9a6526c67b2341fbff059afe80ce21c9cc5781
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 08:13:04 +0200

    Update rotone.c

--- EXAM RANK 2/LEVEL 0/rotone.c
+++ EXAM RANK 2/LEVEL 0/rotone.c
@@ -32,26 +32,31 @@ $>
 #include <unistd.h>
 
 int     main(int ac, char **av)
-{       
+{
         int     i;
-        
+        // Vérification si le nombre d'arguments est égal à 2
         if (ac != 2)
         {
+                // Si ce n'est pas le cas, on écrit un retour à la ligne et on retourne 0
                 write(1, "\n", 1);
                 return (0);
         }
         i = 0;
+        // Tant qu'on n'est pas à la fin de la chaîne de caractères, on continue
         while (av[1][i])
         {
-                if ((av[1][i] >= 'a' && av[1][i] < 'z') ||
-                                (av[1][i] >= 'A' && av[1][i] < 'Z'))
+                // Si le caractère est une lettre entre a et y ou A et Y, on incrémente de 1 dans le code ASCII
+                if ((av[1][i] >= 'a' && av[1][i] < 'z') || (av[1][i] >= 'A' && av[1][i] < 'Z'))
                         av[1][i] += 1;
+                // Si le caractère est z ou Z, on soustrait 25 dans le code ASCII pour revenir à a ou A
                 else if (av[1][i] == 'z' || av[1][i] == 'Z')
                         av[1][i] -= 25;
-                write(1, av[1][i], 1);
+                // On écrit le caractère modifié dans la sortie standard
+                write(1, &av[1][i], 1);
+                // On incrémente le compteur
                 i++;
         }
+        // On écrit un retour à la ligne et on retourne 0
         write(1, "\n", 1);
         return (0);
 }
-
