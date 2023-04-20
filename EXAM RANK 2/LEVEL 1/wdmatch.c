82804a68e6a911350c79ce1f96e7bcbe6fbcf750
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 11:00:13 +0200

    Update wdmatch.c

--- EXAM RANK 2/LEVEL 1/wdmatch.c
+++ EXAM RANK 2/LEVEL 1/wdmatch.c
@@ -27,39 +27,47 @@ $
 
 ------------------------------------------*/
 
-##include <unistd.h>
+#include <unistd.h>
 
 int main(int ac, char **av)
 {
+    int i;
+    int j;
+    
+    // Vérifie s'il y a exactement 2 arguments
     if (ac != 3)
     {
+        // Si ce n'est pas le cas, affiche une nouvelle ligne et quitte le programme
         write(1, "\n", 1);
         return 0;
     }
-
-    int i = 0;
-    int j = 0;
+    i = 0;
+    j = 0;
     while (av[1][i])
     {
+        // Parcourt la deuxième chaîne de caractères jusqu'à trouver le même caractère
+        // que celui de la première chaîne, ou jusqu'à atteindre la fin de la deuxième chaîne
         while (av[2][j] && av[1][i] != av[2][j])
-                j++;
+               j++;
+        // Si on a atteint la fin de la deuxième chaîne sans trouver le caractère, la fonction se termine
         if (av[2][j] == '\0')
         {
             write(1, "\n", 1);
             return 0;
         }
+        // Si le caractère existe dans les deux chaînes, on passe au caractère suivant dans la première chaîne
         i++;
         j++;
     }
-
+    // Si on a atteint cette ligne, c'est que tous les caractères de la première chaîne existent dans la deuxième chaîne
     i = 0;
     while (av[1][i])
     {
+        // Affiche chaque caractère de la première chaîne
         write(1, &av[1][i], 1);
         i++;
     }
+    // Affiche une nouvelle ligne à la fin
     write(1, "\n", 1);
     return 0;
 }
-
-
