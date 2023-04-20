b42f3fe72af9d1a6cccef6f39a535d2486327cfb
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 12:18:49 +0200

    Update tab_mult.c

--- EXAM RANK 2/LEVEL 2/tab_mult.c
+++ EXAM RANK 2/LEVEL 2/tab_mult.c
@@ -42,47 +42,70 @@ $>
 #include <stdlib.h>
 #include <unistd.h>
 
-void ft_putnbr(int nb)
+void    ft_putnbr(int nb)
 {
+    char    c;
+    
+    // Fonction récursive pour afficher un nombre entier en utilisant write
     if (nb >= 10)
+        // On divise nb par 10 pour obtenir le nombre de dizaines, centaines, etc.
         ft_putnbr(nb / 10);
-    char c = nb % 10 + '0';
+    // On calcule le chiffre des unités en faisant nb modulo 10 et en ajoutant le code ASCII pour '0'
+    c = nb % 10 + '0';
+    // On écrit le chiffre des unités
     write(1, &c, 1);
 }
 
 int ft_atoi(char *str)
 {
-    int result = 0;
-    while (*str >= '0' && *str <= '9')
+    // Fonction pour convertir une chaîne de caractères en un entier
+    int result;
+    int i;
+    
+    result = 0;
+    i = 0
+    while (str[i] >= '0' && str[i] <= '9')
     {
-        result = result * 10 + (*str - '0');
-        str++;
+        // On multiplie result par 10 pour décaler les chiffres et on ajoute le chiffre courant de la chaîne
+        result = result * 10 + str[i] - '0';
+        i++;
     }
-    return result;
+    return (result);
 }
 
 void print_table(int n, int i)
 {
+    // Fonction récursive pour afficher la table de multiplication de n jusqu'à 9*n
     if (i <= 9)
     {
+        // On affiche le multiplicateur
         ft_putnbr(i);
+        // On affiche le symbole 'x' pour indiquer la multiplication
         write(1, " x ", 3);
+        // On affiche le multiplicande
         ft_putnbr(n);
+        // On affiche le symbole '=' pour indiquer le résultat
         write(1, " = ", 3);
+        // On affiche le résultat de la multiplication
         ft_putnbr(n * i);
+        // On saute une ligne pour passer à la multiplication suivante
         write(1, "\n", 1);
+        // On appelle la fonction récursivement pour afficher la multiplication suivante
         print_table(n, i + 1);
     }
 }
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    if (argc != 2)
+    if (ac != 2)
     {
+        // Si le nombre d'arguments est incorrect, on affiche une ligne vide et on termine le programme
         write(1, "\n", 1);
         return 0;
     }
-    int n = ft_atoi(argv[1]);
-    print_table(n, 1);
+    // On convertit le premier argument en un entier
+    int n = ft_atoi(av[1]);
+    // On affiche la table de multiplication pour ce nombre
+    print_table(n, 1); 
     return 0;
 }
