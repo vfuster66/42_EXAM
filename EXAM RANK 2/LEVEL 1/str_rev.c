5032a7826a97222a39b2d6afa2b97ddd89e8131e
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 10:50:38 +0200

    Update str_rev.c

--- EXAM RANK 2/LEVEL 1/str_rev.c
+++ EXAM RANK 2/LEVEL 1/str_rev.c
@@ -19,23 +19,36 @@ char	*str_rev(char *str)
 {
 	char	*start;
 	char	*end;
+	char	temp;
 
+	// si la chaîne de caractères est nulle
 	if (!str)
 		return (NULL);
+	// on pointe sur le début de la chaîne de caractères
 	start = str;
+	// on pointe sur le début de la chaîne de caractères
 	end = str;
+	// tant qu'on n'a pas atteint la fin de la chaîne de caractères
 	while (*end != '\0')
+		// on avance jusqu'à la fin de la chaîne de caractères
 		end++;
+	// on se place sur le dernier caractère de la chaîne de caractères
 	end--;
-	char	temp;
-	while (start < end)
+	// tant que le pointeur de début est avant le pointeur de fin
+	while (start < end) 
 	{
+		// on stocke le caractère pointé par le pointeur de début dans la variable temp
 		temp = *start;
+		// on remplace le caractère pointé par le pointeur de début par le caractère pointé par le pointeur de fin
 		*start = *end;
+		// on remplace le caractère pointé par le pointeur de fin par le caractère stocké dans la variable temp
 		*end = temp;
+		// on avance le pointeur de début
 		start++;
+		// on recule le pointeur de fin
 		end--;
 	}
+	// on retourne la chaîne de caractères inversée
 	return (str);
 }
 
