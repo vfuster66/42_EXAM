5177617ef52cdebb5c18fdd75eb3709b91bad3a0
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 08:17:41 +0200

    Update ulstr.c

--- EXAM RANK 2/LEVEL 0/ulstr.c
+++ EXAM RANK 2/LEVEL 0/ulstr.c
@@ -30,19 +30,26 @@ int	main(int ac, char **av)
 	int	i;
 
 	i = 0;
+	// verifie que le nombre d'arguments est bien egal a 2
 	if (ac == 2)
 	{
+		// parcourt chaque caractère de la chaîne de caractères passée en argument
 		while (av[1][i])
 		{
+			// si le caractère est une lettre minuscule, le transforme en majuscule
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
 				av[1][i] -= 32;
+			// sinon, si le caractère est une lettre majuscule, le transforme en minuscule
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
 				av[1][i] += 32;
+			// écrit le caractère transformé dans la sortie standard
 			write(1, &av[1][i], 1);
 			i++;
 		}	
 	}
+	// écrit un retour à la ligne dans la sortie standard
 	write(1, "\n", 1);
 	return (0);
 }
+
 
