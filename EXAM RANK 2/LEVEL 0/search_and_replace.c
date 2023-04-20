e3c7ff39e11b400cb381398f090ef5f7af2b2d78
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 08:16:05 +0200

    Update search_and_replace.c

--- EXAM RANK 2/LEVEL 0/search_and_replace.c
+++ EXAM RANK 2/LEVEL 0/search_and_replace.c
@@ -35,16 +35,21 @@ int	main(int ac, char **av)
 	int	i;
 
 	i = 0;
+	// Vérification que le programme a 3 arguments et que les deux derniers sont des caractères
 	if (ac == 4 && av[2][1] == '\0' && av[3][1] == '\0')
 	{
+		// Parcours de la chaîne de caractères passée en premier argument
 		while (av[1][i])
 		{
+			// Si le caractère actuel est égal au caractère de remplacement
 			if (av[1][i] == av[2][0])
-				av[1][i] = av[3][0];
+				// Remplacement du caractèr
+				av[1][i] = av[3][0]; e
 			write(1, &av[1][i], 1);
 			i++;
 		}
 	}
+	// Ajout d'un retour à la ligne à la fin de l'exécution
 	write(1, "\n", 1);
+	return (0);
 }
-
