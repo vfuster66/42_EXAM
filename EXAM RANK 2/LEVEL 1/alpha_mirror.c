affdc3d7b0f8139538dac6e8dbc95f74cb068ecc
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 08:20:46 +0200

    Update alpha_mirror.c

--- EXAM RANK 2/LEVEL 1/alpha_mirror.c
+++ EXAM RANK 2/LEVEL 1/alpha_mirror.c
@@ -33,21 +33,30 @@ $>
 int	main(int ac, char **av)
 {
 	int	i;
-
+	
 	i = 0;
-	if (ac == 2)
+	// Vérification que le programme est appelé avec exactement un argument
+	if (ac == 2) 
 	{
+		// Boucle sur tous les caractères de l'argument
 		while (av[1][i])
 		{
+			// Si le caractère est une minuscule
 			if (av[1][i] >= 'a' && av[1][i] <= 'z')
+				// Conversion du caractère en "complément à 219"
 				av[1][i] = 219 - av[1][i];
+			// Si le caractère est une majuscule
 			else if (av[1][i] >= 'A' && av[1][i] <= 'Z')
+				// Conversion du caractère en "complément à 155"
 				av[1][i] = 155 - av[1][i];
+			// Écriture du caractère dans la sortie standard
 			write(1, &av[1][i], 1);
+			// Incrémentation de i pour passer au caractère suivant
 			i++;
 		}
 	}
+	// Écriture d'un retour à la ligne à la fin de la sortie standard
 	write(1, "\n", 1);
+	// Retourne 0 pour indiquer que le programme s'est exécuté correctement
 	return (0);
 }
-
