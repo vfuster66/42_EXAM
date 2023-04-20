06bf72eaadedf1a919fe7f81a1ef48ae61ae9142
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 10:43:34 +0200

    Update reverse_bits.c

--- EXAM RANK 2/LEVEL 1/reverse_bits.c
+++ EXAM RANK 2/LEVEL 1/reverse_bits.c
@@ -16,19 +16,27 @@ _____________
 
 -----------------------------------------------*/
 
-unsigned char	reverse_bits(unsigned char octet)
+// fonction pour inverser les bits d'un octet
+unsigned char reverse_bits(unsigned char octet)
 {
+	// octet inversé
 	unsigned char	reversed;
-	int				i;
+	// compteur
+	int				i; 
 
 	reversed = 0;
 	i = 0;
-	while (i < 8)
+	// pour chaque bit de l'octet
+	while (i < 8) 
 	{
+		// on décale l'octet inversé d'un bit vers la gauche et on y ajoute le bit courant
 		reversed = (reversed << 1) | (octet & 1);
+		// on décale l'octet original d'un bit vers la droite pour passer au bit suivant
 		octet >>= 1;
-		i++;
+		// on incrémente le compteur pour passer au bit suivant
+		i++; 
 	}
+	// on retourne l'octet inversé
 	return (reversed);
 }
 
