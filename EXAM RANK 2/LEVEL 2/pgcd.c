131082ba9bb221dcd2c77a8932357cd0423cb490
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-04-20 11:52:02 +0200

    Update pgcd.c

--- EXAM RANK 2/LEVEL 2/pgcd.c
+++ EXAM RANK 2/LEVEL 2/pgcd.c
@@ -29,30 +29,39 @@ $
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Cette fonction calcule le PGCD (Plus Grand Commun Diviseur) de deux entiers
+   passés en argument en ligne de commande, et l'affiche sur la sortie standard. */
 int     main(int ac, char **av)
 {
         int     a;
         int     b;
 
-        if (ac != 3)
+        // si le nombre d'arguments est différent de 3, on affiche une ligne vide et on termine le programme
+        if (ac != 3)	
         {
                 printf("\n");
                 return (0);
         }
+        // on convertit la première chaîne de caractères en entier
         a = atoi(av[1]);
+        // on convertit la deuxième chaîne de caractères en entier
         b = atoi(av[2]);
+        // tant que les deux entiers sont différents...
         while (a != b)
         {
+                // si le premier est plus grand que le deuxième...
                 if (a > b)
                 {
+                        // on lui soustrait le deuxième
                         a = a - b;
                 }
                 else
                 {
+                        // on soustrait le premier au deuxième
                         b = b - a;
                 }
         }
-        printf("%d\n", a);
+        // on affiche le PGCD (qui est dans a) sur la sortie standard
+        printf("%d\n", a);	
         return (0);
 }
-
