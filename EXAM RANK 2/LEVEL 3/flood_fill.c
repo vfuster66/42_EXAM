e42153c8141f5441f9cc37d70d8c3766cfd19129
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-05-04 15:57:31 +0200

    Update flood_fill.c

--- EXAM RANK 2/LEVEL 3/flood_fill.c
+++ EXAM RANK 2/LEVEL 3/flood_fill.c
@@ -102,9 +102,9 @@ typedef struct s_point
 
 void	fill(char **tab, t_point size, t_point cur, char to_fill)
 {
+	// Arrête si on est hors des limites ou si la case n'a pas la valeur to_fill
 	if (cur.y < 0 || cur.y >= size.y || cur.x < 0 || cur.x >= size.x
-		|| tab[cur.y][cur.x] != to_fill)
-		// Arrête si on est hors des limites ou si la case n'a pas la valeur to_fill
+			|| tab[cur.y][cur.x] != to_fill)
 		return ;
 	// Remplace la case courante par 'F'
 	tab[cur.y][cur.x] = 'F'; 
