a67860a7793afcfea0ed1ea33e75dae808d6273d
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-05-04 08:08:13 +0200

    Update do_op.c

--- EXAM RANK 2/LEVEL 1/do_op.c
+++ EXAM RANK 2/LEVEL 1/do_op.c
@@ -29,87 +29,25 @@ $
 
 -----------------------------------*/
 
-#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int	ft_atoi(char *str)
-{
-	// Initialisation de variables
-	int	result;
-	int	sign;
-	int	i;
-	
-	i = 0;
-	sign = 1;
-	result = 0;
-	// Gestion du signe éventuel
-	if (str[i] == '-')
+int main(int argc, char *argv[])
+{	
+	// On verifie qu'il y ait bien 3 arguments
+	if (argc == 4)
 	{
-		sign = -1;
-		i++;
+		if (argv[2][0] == '+')
+			printf("%d", (atoi(argv[1]) + atoi(argv[3])));
+		if (argv[2][0] == '-')
+			printf("%d", (atoi(argv[1]) - atoi(argv[3])));
+		if (argv[2][0] == '*')
+			printf("%d", (atoi(argv[1]) * atoi(argv[3])));
+		if (argv[2][0] == '/')
+			printf("%d", (atoi(argv[1]) / atoi(argv[3])));
+		if (argv[2][0] == '%')
+			printf("%d", (atoi(argv[1]) % atoi(argv[3])));
 	}
-	// Parcours de la chaîne de caractères
-	while (str[i])
-	{
-		// Conversion de chaque chiffre en entier et construction du nombre
-		result = result * 10 + str[i] - '0';
-		i++;
-	}
-	// Retourne le nombre final avec le signe approprié
-	return (result * sign);
-}
-
-void	ft_putnbr(int n)
-{
-	char	c;
-	
-	// Si l'entier est négatif, on affiche le signe '-' et on travaille avec la valeur absolue
-	if (n < 0) 
-	{
-		write(1, "-", 1);
-		n = -n;
-	}
-	// Si l'entier est supérieur à 9, on continue à récursivement afficher les chiffres
-	if (n > 9)
-	{
-		ft_putnbr(n / 10);
-	}
-	// Conversion du chiffre en caractère
-	c = (n % 10) + '0';
-	// Affichage du caractère
-	write(1, &c, 1);
-}
-
-int	main(int ac, char **av) 
-{
-	// Vérification du nombre d'arguments
-	if (ac != 4) 
-	{
-		write(1, "\n", 1);
-		return 0;
-    }
-    // Conversion des deux opérandes en entiers et récupération de l'opérateur
-	int nb1 = ft_atoi(av[1]);
-	int nb2 = ft_atoi(av[3]);
-	char op = av[2][0];
-	int	result;
-	
-	// Calcul du résultat en fonction de l'opérateur
-	if (op == '+')
-		result = nb1 + nb2;
-	else if (op == '-')
-		result = nb1 - nb2;
-	else if (op == '*') 
-		result = nb1 * nb2;
-	else if (op == '/') 
-		result = nb1 / nb2;
-	else if (op == '%') 
-		result = nb1 % nb2;
-	else
-		result = 0;
-	// Si le résultat n'est pas 0, on l'affiche
-	if (result != 0) 
-		ft_putnbr(result);
-	// On affiche un retour à la ligne à la fin
-	write(1, "\n", 1); 
-	return 0;
+	printf("\n");
+	return (0);
 }
