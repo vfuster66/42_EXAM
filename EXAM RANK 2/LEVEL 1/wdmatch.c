43e5d98d4a48842c5c985bb37713c9fa61a5f972
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-05-11 11:18:46 +0200

    Update wdmatch.c

--- EXAM RANK 2/LEVEL 1/wdmatch.c
+++ EXAM RANK 2/LEVEL 1/wdmatch.c
@@ -29,45 +29,46 @@ $
 
 #include <unistd.h>
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
     int i;
     int j;
-    
-    // Vérifie s'il y a exactement 2 arguments
-    if (ac != 3)
+    // Vérifier qu'il y a 2 arguments en ligne de commande
+    if (argc != 3)
     {
-        // Si ce n'est pas le cas, affiche une nouvelle ligne et quitte le programme
+        // Afficher un saut de ligne et quitter
         write(1, "\n", 1);
-        return 0;
+        return (0);
     }
+    // Initialiser les compteurs pour les deux chaînes
     i = 0;
     j = 0;
-    while (av[1][i])
+    // Parcourir les deux chaînes caractère par caractère
+    while (argv[1][i] && argv[2][j])
     {
-        // Parcourt la deuxième chaîne de caractères jusqu'à trouver le même caractère
-        // que celui de la première chaîne, ou jusqu'à atteindre la fin de la deuxième chaîne
-        while (av[2][j] && av[1][i] != av[2][j])
-               j++;
-        // Si on a atteint la fin de la deuxième chaîne sans trouver le caractère, la fonction se termine
-        if (av[2][j] == '\0')
+        // Si les deux caractères sont identiques, avancer dans la première chaîne
+        if (argv[1][i] == argv[2][j])
         {
-            write(1, "\n", 1);
-            return 0;
+            i++;
         }
-        // Si le caractère existe dans les deux chaînes, on passe au caractère suivant dans la première chaîne
-        i++;
+        // Avancer dans la deuxième chaîne dans tous les cas
         j++;
     }
-    // Si on a atteint cette ligne, c'est que tous les caractères de la première chaîne existent dans la deuxième chaîne
-    i = 0;
-    while (av[1][i])
+    // Si on est arrivé à la fin de la première chaîne, cela signifie qu'on a trouvé
+    // tous les caractères nécessaires dans la deuxième chaîne dans le bon ordre
+    if (argv[1][i] == '\0')
     {
-        // Affiche chaque caractère de la première chaîne
-        write(1, &av[1][i], 1);
-        i++;
+        // Réinitialiser le compteur pour la première chaîne
+        i = 0;
+        // Afficher chaque caractère de la première chaîne un par un
+        while (argv[1][i])
+        {
+            write(1, &argv[1][i], 1);
+            i++;
+        }
     }
-    // Affiche une nouvelle ligne à la fin
+    // Afficher un saut de ligne et quitter
     write(1, "\n", 1);
-    return 0;
+    return (0);
 }
+
