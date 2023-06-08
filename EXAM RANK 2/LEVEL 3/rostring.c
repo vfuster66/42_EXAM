ea510b3332d7a43c5791cd7ebd300c24bd72e49e
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-06-08 16:15:59 +0200

    Update rostring.c

--- EXAM RANK 2/LEVEL 3/rostring.c
+++ EXAM RANK 2/LEVEL 3/rostring.c
@@ -35,80 +35,63 @@ $>
 
 --------------------------------*/
 
-#include <unistd.h>
-#include <stdlib.h>
+#include <unistd.h>  // Bibliothèque pour utiliser la fonction write
+#include <stdlib.h>  // Bibliothèque standard du C
 
 int main(int ac, char **av)
 {
-	// Vérifier que l'argument est bien présent
-    	if (ac < 2) 
-    	{
-		// Si l'argument est absent, écrire une ligne vide et terminer le programme
-        	write(1, "\n", 1);
-        	return 0;
-    	}
-    	// Trouver le début du premier mot
-    	int	i;
+    int i = 0;
+    int start;
+    int end;
+    int flag;
+
+    if (ac > 1 && av[1][0])
+    {
+        // Recherche des espaces ou tabulations au début du deuxième argument
+        while (av[1][i] == ' ' || av[1][i] == '\t')
+            i++;
+        start = i;
+
+        // Recherche de la fin du premier mot dans le deuxième argument
+        while (av[1][i] != ' ' && av[1][i] != '\t' && av[1][i] != '\0')
+            i++;
+        end = i;
+
+        // Recherche des espaces ou tabulations après la fin du premier mot
+        while (av[1][i] == ' ' || av[1][i] == '\t')
+            i++;
+
+        // Affichage des mots sans espaces/tabs consécutifs
+        while (av[1][i])
+        {
+            // Ignorer les espaces/tabs consécutifs
+            while ((av[1][i] == ' ' && av[1][i + 1] == ' ') || (av[1][i] == '\t' && av[1][i + 1] == '\t'))
+                i++;
+
+            // Vérification si l'espace ou la tabulation est présente
+            if (av[1][i] == ' ' || av[1][i] == '\t')
+                flag = 1;
+
+            // Affichage du caractère courant
+            write(1, &av[1][i], 1);
+            i++;
+        }
+
+        // Affichage d'un espace si nécessaire
+        if (flag)
+            write(1, " ", 1);
+
+        // Affichage du premier mot
+        while (start < end)
+        {
+            write(1, &av[1][start], 1);
+            start++;
+        }
+    }
+
+    // Affichage d'une nouvelle ligne
+    write(1, "\n", 1);
+
+    return (0);
+}
 	
-	i = 0;
-	// Chercher les espaces ou les tabulations au début de la chaîne
-    	while (av[1][i] == ' ' || av[1][i] == '\t') 
-        i++;
-    	// Trouver la fin du premier mot
-    	int	j;
-	
-	j = i;
-	// Chercher la fin du premier mot (la première espace ou tabulation)
-    	while (av[1][j] && av[1][j] != ' ' && av[1][j] != '\t') 
-        	j++;
-    	// Sauvegarder le premier mot dans un tampon temporaire
-    	int	len;
-	
-	// Calculer la longueur du premier mot
-	len = j - i;
-	// Allouer de la mémoire pour stocker le premier mot (plus un caractère pour le terminateur de chaîne)
-    	char *tmp = malloc(len + 1);
-	// Vérifier que l'allocation a réussi
-    	if (!tmp)
-        	return 1;
-    	int	k;
-	
-	k = -1;
-	// Copier le premier mot dans le tampon temporaire
-    	while (++k < len)
-		tmp[k] = av[1][i + k];
-	// Terminer la chaîne avec le caractère nul
-    	tmp[k] = '\0';
-    	// Imprimer le reste de la chaîne avec des mots séparés par des espaces
-	// Ignorer les espaces ou les tabulations qui suivent le premier mot
-    	while (av[1][j] && (av[1][j] == ' ' || av[1][j] == '\t'))
-        	j++;
-	// Parcourir le reste de la chaîne
-    	while (av[1][j])
-	{ 
-		// Début d'un nouveau mot
-        	i = j; 
-		// Chercher la fin du mot
-        	while (av[1][j] && av[1][j] != ' ' && av[1][j] != '\t') 
-            		j++;
-		// Si un mot a été trouvé, l'écrire
-        	if (i < j)
-		{ 
-			// Écrire un espace avant le mot
-            		write(1, " ", 1);
-			// Écrire le mot
-            		write(1, av[1] + i, j - i); 
-        	}
-		// Ignorer les espaces ou les tabulations qui suivent le mot
-        	while (av[1][j] && (av[1][j] == ' ' || av[1][j] == '\t')) 
-            		j++;
-    	}
-    	// Imprimer le premier mot à la fin, avec un espace
-    	write(1, " ", 1);
-    	write(1, tmp, len);
-    	// Libérer le tampon temporaire
-    	free(tmp);
-    	// Écrire une ligne vide pour terminer la sortie
-    	write(1, "\n", 1);
-    	return 0;
-}	
