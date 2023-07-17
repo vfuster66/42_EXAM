a4eda7af0099b206d411b94b04c72cd7ccaebf0e
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-07-17 14:14:03 +0200

    Update rstr_capitalizer.c

--- exam rank 02 - solutions/rstr_capitalizer.c
+++ exam rank 02 - solutions/rstr_capitalizer.c
@@ -12,31 +12,29 @@
 
 #include <unistd.h>
 
-int main(int ac, char **av)
+int		main(int ac, char **av)
 {
-	int	i = 1;
-	int	j;
+	int		i;
+	int		j;
 
-	if (ac != 1)
+	i = 1;
+	while (i < ac)
 	{
-		while (i < ac)
-		{	
-			j = 0;
-			if (av[i][j] >= 'a' && av[i][j] <= 'z')
+		j = 0;
+		while (av[i][j])
+		{
+			if (av[i][j] >= 'A' && av[i][j] <= 'Z')
+				av[i][j] += 32;
+			if ((av[i][j] >= 'a' && av[i][j] <= 'z') 
+					&& (av[i][j + 1] == ' ' 
+					|| av[i][j + 1] == '\t' 
+					|| av[i][j + 1] == '\0'))
 				av[i][j] -= 32;
 			write(1, &av[i][j], 1);
-			while (av[i][++j])
-			{
-				if (av[i][j] >= 'A' && av[i][j] <= 'Z')
-					av[i][j] += 32;
-				if ((av[i][j] >= 'a' && av[i][j] <= 'z') && (av[i][j + 1] == ' ' || av[i][j + 1] == '\t' || av[i][j + 1] == '\0'))
-					av[i][j] -= 32;
-				write(1, &av[i][j], 1);
-				
-			}
-			write(1, "\n", 1);
-			i++;
+			j++;
 		}
+		write(1, "\n", 1);
+		i++;
 	}
 	return (0);
 }
