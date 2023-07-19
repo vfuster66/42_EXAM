b445a24fa6da23fe1b3395abdb1d9c4bdad115d5
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-07-19 13:00:18 +0200

    Update do_op.c

--- exam rank 02 - entrainement/do_op.c
+++ exam rank 02 - entrainement/do_op.c
@@ -15,19 +15,5 @@
 
 int	main(int ac, char **av)
 {
-	if (ac == 4)
-	{
-		if (av[2][0] == '+')
-			printf("%d", atoi(av[1]) + atoi(av[3]));
-		if (av[2][0] == '-')
-			printf("%d", atoi(av[1]) - atoi(av[3]));
-		if (av[2][0] == '*')
-			printf("%d", atoi(av[1]) * atoi(av[3]));
-		if (av[2][0] == '/')
-			printf("%d", atoi(av[1]) / atoi(av[3]));
-		if (av[2][0] == '%')
-			printf("%d", atoi(av[1]) % atoi(av[3]));
-	}
-	write(1, "\n", 1);;
-	return (0);
+
 }
