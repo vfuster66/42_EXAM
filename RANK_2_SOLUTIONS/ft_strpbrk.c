3862b3e3a9581f3dbe492c3ed72eef96e885036b
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 08:57:45 +0200

    Update ft_strpbrk.c

--- RANK_2_SOLUTIONS/ft_strpbrk.c
+++ RANK_2_SOLUTIONS/ft_strpbrk.c
@@ -10,7 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+/*Assignment name	: ft_strpbrk
+Expected files	: ft_strpbrk.c
+Allowed functions: None
+---------------------------------------------------------------
+
+Reproduce exactly the behavior of the function strpbrk
+(man strpbrk).
+
+The function should be prototyped as follows:
+
+char	*ft_strpbrk(const char *s1, const char *s2);
+
+--------------------------------------*/
+
+#include <stdlib.h>
 
 char	*ft_strpbrk(const char *s1, const char *s2)
 {
