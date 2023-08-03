2e3a8bb336ac2bf55a8f3cde81f64385953e5259
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 08:56:08 +0200

    Update ft_strcspn.c

--- RANK_2_SOLUTIONS/ft_strcspn.c
+++ RANK_2_SOLUTIONS/ft_strcspn.c
@@ -10,6 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name	: ft_strcspn
+Expected files	: ft_strcspn.c
+Allowed functions: None
+---------------------------
+
+Reproduce exactly the behavior of the function strcspn
+(man strcspn).
+
+The function should be prototyped as follows:
+
+size_t	ft_strcspn(const char *s, const char *reject);
+
+--------------------------*/
+
 #include <stdlib.h>
 
 size_t	ft_strcspn(const char *s, const char *reject)
