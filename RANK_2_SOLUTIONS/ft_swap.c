dd400a8879d0f998f1b547bb5a7bbab5a36ef3f1
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 09:19:39 +0200

    Update ft_swap.c

--- RANK_2_SOLUTIONS/ft_swap.c
+++ RANK_2_SOLUTIONS/ft_swap.c
@@ -10,6 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name  : ft_swap
+Expected files   : ft_swap.c
+Allowed functions: 
+--------------------------
+
+Write a function that swaps the contents
+of two integers the adresses of which are 
+passed as parameters.
+
+Your function must be declared as follows:
+
+void	ft_swap(int *a, int *b);
+
+-----------------------*/
+
 void	ft_swap(int *a, int *b)
 {
 	int	temp;
