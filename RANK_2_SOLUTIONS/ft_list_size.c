5b1b24f7d265d665fde43d23e728eb3db28be5ad
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 09:07:52 +0200

    Update ft_list_size.c

--- RANK_2_SOLUTIONS/ft_list_size.c
+++ RANK_2_SOLUTIONS/ft_list_size.c
@@ -10,6 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name  : ft_list_size
+Expected files   : ft_list_size.c
+Allowed functions: 
+---------------------------------
+
+Write a function that returns the number of elements in the linked list that's
+passed to it.
+
+It must be declared as follows:
+
+int	ft_list_size(t_list *begin_list);
+
+You must use the following structure in your program ft_list_size.c :
+
+typedef struct    s_list
+{
+    struct s_list *next;
+    void          *data;
+}                 t_list;
+
+------------------------------*/
 
 // Version 1
 #include <stdlib.h>
