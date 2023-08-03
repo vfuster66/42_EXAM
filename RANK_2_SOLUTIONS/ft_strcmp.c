513bb0ca3802a6235c1b8776f1d1c105c13d9684
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 08:55:33 +0200

    Update ft_strcmp.c

--- RANK_2_SOLUTIONS/ft_strcmp.c
+++ RANK_2_SOLUTIONS/ft_strcmp.c
@@ -10,6 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name  : ft_strcmp
+Expected files   : ft_strcmp.c
+Allowed functions:
+-----------------------------------
+
+Reproduce the behavior of the function strcmp (man strcmp).
+
+Your function must be declared as follows:
+
+int    ft_strcmp(char *s1, char *s2);
+
+------------------------------------*/
+
 int	ft_strcmp(const char *s1, const char *s2)
 {
     int i = 0;
