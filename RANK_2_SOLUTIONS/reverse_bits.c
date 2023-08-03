98ed090ae9c034039fe2ba11446b53353784d248
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 09:03:56 +0200

    Update reverse_bits.c

--- RANK_2_SOLUTIONS/reverse_bits.c
+++ RANK_2_SOLUTIONS/reverse_bits.c
@@ -10,6 +10,24 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Write a function that takes a byte, reverses it, bit by bit (like the
+example) and returns the result.
+
+Your function must be declared as follows:
+
+unsigned char	reverse_bits(unsigned char octet);
+
+Example:
+
+  1 byte
+_____________
+ 0100  0001
+     ||
+     \/
+ 1000  0010
+
+-----------------------------------------------*/
+
 unsigned char	reverse_bits(unsigned char octet)
 {
 	int	i = 0;
