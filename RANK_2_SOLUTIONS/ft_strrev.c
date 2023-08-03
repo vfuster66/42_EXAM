4462f3f6c66591c4e4d812ef8f5436b15e86de5a
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 08:58:08 +0200

    Update ft_strrev.c

--- RANK_2_SOLUTIONS/ft_strrev.c
+++ RANK_2_SOLUTIONS/ft_strrev.c
@@ -10,6 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name  : ft_strrev
+Expected files   : ft_strrev.c
+Allowed functions: 
+--------------------------------------------------------------------------------
+
+Écrire une fonction qui inverse (en place) une chaîne de caractères.
+
+Elle devra renvoyer son paramètre.
+
+Votre fonction devra être prototypée de la façon suivante :
+
+char    *ft_strrev(char *str);
+
+----------------------------------------*/
+
 #include <stdlib.h>
 
 char    *ft_strrev(char *str)
