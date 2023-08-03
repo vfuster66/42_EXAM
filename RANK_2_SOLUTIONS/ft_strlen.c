3b24b8120c511df0c862d3ebf4b40a010041816b
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 09:19:13 +0200

    Update ft_strlen.c

--- RANK_2_SOLUTIONS/ft_strlen.c
+++ RANK_2_SOLUTIONS/ft_strlen.c
@@ -10,6 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name  : ft_strlen
+Expected files   : ft_strlen.c
+Allowed functions:
+-------------------------------
+
+Write a function that returns the length of a string.
+
+Your function must be declared as follows:
+
+int	ft_strlen(char *str);
+
+---------------------------------*/
+
 int	ft_strlen(char *str)
 {
 	int	i = 0;
