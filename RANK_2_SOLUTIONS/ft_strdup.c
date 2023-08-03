aae17bbdffa750023045f6febe63fe7e869648ce
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 08:56:45 +0200

    Update ft_strdup.c

--- RANK_2_SOLUTIONS/ft_strdup.c
+++ RANK_2_SOLUTIONS/ft_strdup.c
@@ -10,6 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name  : ft_strdup
+Expected files   : ft_strdup.c
+Allowed functions: malloc
+--------------------------------------------------------------------------------
+
+Reproduce the behavior of the function strdup (man strdup).
+
+Your function must be declared as follows:
+
+char    *ft_strdup(char *src);
+
+---------------------------------------------*/
+
 #include <stdlib.h>
 
 char    *ft_strdup(char *src)
