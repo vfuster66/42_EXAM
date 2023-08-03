2ddb5dbdc6dc727879617cb7b780c5f357e2cc00
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-03 09:05:24 +0200

    Update union.c

--- RANK_2_SOLUTIONS/union.c
+++ RANK_2_SOLUTIONS/union.c
@@ -10,6 +10,36 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*Assignment name  : union
+Expected files   : union.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes two strings and displays, without doubles, the
+characters that appear in either one of the strings.
+
+The display will be in the order characters appear in the command line, and
+will be followed by a \n.
+
+If the number of arguments is not 2, the program displays \n.
+
+Example:
+
+$>./union zpadinton "paqefwtdjetyiytjneytjoeyjnejeyj" | cat -e
+zpadintoqefwjy$
+$>./union 
+df6vewg4thras$
+$>./union "rien" "cette phrase ne cache rien" | cat -e
+rienct phas$
+$>./union | cat -e
+$
+$>
+$>./union "rien" | cat -e
+$
+$>
+
+-------------------------------*/
+
 #include <unistd.h>
 
 int	main(int ac, char **av)
