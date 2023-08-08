66ffb3bfabe19bb3f621377422f358afd270c7c7
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-08 12:11:51 +0200

    Update ft_itoa.c

--- RANK_2_SOLUTIONS/ft_itoa.c
+++ RANK_2_SOLUTIONS/ft_itoa.c
@@ -29,32 +29,47 @@ char	*ft_itoa(int nbr);
 
 char    *ft_itoa(int nbr)
 {
+	// Index pour la construction de la chaine de caracteres
 	int		i = 0;
+	// Variable temporaire utilisee pour determiner le nombre de 
+	// chiffres dans nbr
 	long	temp = nbr;
+	// Pointeur vers un caractere = tableau de caracteres pour stocker 
+	// la chaine de caracteres
 	char	*str;
 
+	// INT_MIN  = Seul cas ou la conversion necessite un traitement special
 	if (nbr == -2147483648)
 		return ("-2147483648");
+	// Allocation dynamique de la memoire pour le tableau de caracteres 
 	str = malloc(sizeof(char) * i + 1);
 	if (!str)
 		return (NULL);
+	// Terminer correctement la chaine de caracteres creee a partir de l'entier converti
 	str[i] ='\0';
+	// Si le nombre est egal a 0, on revoie la chaine '0'
 	if (nbr == 0)
 	{
 		str[0] = '0';
 		return (str);
 	}
+	// Si le nombre est negatif
 	if (nbr < 0)
 	{
+		// Passer au caractere suivant = ignorer le caractere '-'
 		i++;
+		// Rendre le nombre positif
 		nbr *= -1;
+		// le premier caractere de la chaine sera le caractere '-'
 		str[0] = '-';
 	}
+	// Boucle pour calculer le nombre de chiffres dans nbr
 	while (temp)
 	{
 		temp /= 10;
 		i++;
 	}
+	// Boucle pour convertir  nbr en chaine de caracteres
 	while (nbr)
 	{
 		str[--i] = (nbr % 10) + '0';
