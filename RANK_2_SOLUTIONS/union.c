e0544ca0aefb0d6d71a3949611f15c17a9e74cfd
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-08 22:19:06 +0200

    Update union.c

--- RANK_2_SOLUTIONS/union.c
+++ RANK_2_SOLUTIONS/union.c
@@ -44,34 +44,50 @@ $>
 
 int	main(int ac, char **av)
 {
+	// Index pour parcourir les chaines de caracteres
 	int	i = 0;
+	// Tableau de 256 elements pour garder une trace des elements deja rencontres
 	int	tab[256];
 
+	// Initialiser toutes les valeurs du tableau a 0
 	if (i < 256)
 	{
 		tab[i] = 0;
 		i++;
 	}
+	// Verifier le nombre exact des arguments
 	if (ac == 3)
 	{
+		// Reinitialiser i
 		i = 0;
+		// Boucle pour parcourir la 1ere chaine de caracteres jusqu'a la fin
 		while (av[1][i])
 		{
+			// Si le caractere actuel n'a jamais ete rencontree
 			if (tab[(unsigned char)av[1][i]] == 0)
 			{
+				// Mise a jour de tab pour dire que le caractere a ete rencontre
 				tab[(unsigned char)av[1][i]] = 1;
+				// Afficher le caractere actuel
 				write(1, &av[1][i], 1);
 			}
+			// Passer au caratere suivant
 			i++;
 		}
+		// Reinitialiser i
 		i = 0;
+		// Boucle pour parcourir la 2eme chaine de caracteres
 		while (av[2][i])
 		{
+			// Si le caractere actuel n'a jamais ete rencontree
 			if (tab[(unsigned char)av[2][i]] == 0)
 			{
+				// Mise a jour de tab pour dire que le caractere a ete rencontre
 				tab[(unsigned char)av[2][i]] = 1;
+				// Afficher le caractere actuel
 				write(1, &av[2][i], 1);
 			}
+			// Passer au caractere suivant
 			i++;
 		}
 	}
