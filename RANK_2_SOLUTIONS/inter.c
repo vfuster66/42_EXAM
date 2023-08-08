f8a0ed17fc95be2935cebaca597269a84dd59af6
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-08 16:43:17 +0200

    Update inter.c

--- RANK_2_SOLUTIONS/inter.c
+++ RANK_2_SOLUTIONS/inter.c
@@ -40,30 +40,44 @@ $
 
 int	main(int ac, char **av)
 {
+	// Index pour parcourir les chaines
 	int	i = 0;
+	// Tableau de 256 elements pour stocker les caracteres presents de la
+	// 2eme chaine
 	int	tab[256];
 
+	// Boucle pour initialiser tous les elements du tableau a 0 
 	while (i < 256)
 	{
 		tab[i] = 0;
 		i++;
 	}
+	// Verifier le nombre exact d'arguments
 	if (ac == 3)
 	{
+		// Reinitialiser i
 		i = 0;
+		// Boucle pour parcourir av[2]
 		while (av[2][i])
 		{
+			// Marquer la presence du caractere dans tab
 			tab[(unsigned char)av[2][i]] = 1;
 			i++;
 		}
+		// Reinitialiser i
 		i = 0;
+		// Boucle pour parcourir av[1]
 		while (av[1][i])
 		{
+			// Si le caractere actuel de av[1] est present dans av[2]
 			if (tab[(unsigned char)av[1][i]] == 1)
 			{
+				// Afficher le caractere actuel
 				write(1, &av[1][i], 1);
+				// Marquer la presence du caractere dans tab pour eviter les doublons
 				tab[(unsigned char)av[1][i]] = 2;
 			}
+			// Passer au caractere suivant
 			i++;
 		}
 	}
