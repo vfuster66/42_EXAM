070e153d6da70eb4ad966d8a5b281600c4452d8d
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-08-08 21:01:52 +0200

    Update rostring.c

--- RANK_2_SOLUTIONS/rostring.c
+++ RANK_2_SOLUTIONS/rostring.c
@@ -51,33 +51,48 @@ $>
 
 int	main(int ac, char **av)
 {
+	// Index pour parcourir la chaine de caracteres
 	int	i = 0;
+	// Variable pour definir le début du 1er mot
 	int	start;
+	// Variable pour definir la fin du 1er mot
 	int	end;
-	int	space;
+	// Variable pour marquer la presence d'un espace
+	int	space = 0;
 
+	// Verifier s'il y a plus d'un argument et que la chaine de caracteres n'est pas vide
 	if (ac > 1 && av[1][0])
 	{
+		// Boucle pour ignorer les espaces et les tabs
 		while (av[1][i] == ' ' || av[1][i] == '\t')
 			i++;
+		// Marquer le debut du 1er mot
 		start = i;
+		// Boucle pour trouver la fin du 1er mot
 		while (av[1][i] != ' ' && av[1][i] != '\t' && av[1][i] != '\0')
 			i++;
+		// Marquer la fin du 1er mot
 		end = i;
+		// Boucle pour ignorer les espaces et les tabs apres le 1er mot
 		while (av[1][i] == ' ' || av[1][i] == '\t')
 			i++;
+		// Boucle pour parcourir la chaine de caracteres jusqu'a la fin
 		while (av[1][i])
 		{
+			// Boucle pour ignorer les espaces et les tabs consecutifs
 			while ((av[1][i] == ' ' && av[1][i + 1] == ' ') 
 					|| (av[1][i] == '\t' && av[1][i + 1] == '\t'))
 				i++;
+			// Si le caractere actuel est un espace ou une tab, mise a jour de space a 1
 			if (av[1][i] == ' ' || av[1][i] == '\t')
 				space = 1;
 			write(1, &av[1][i], 1);
 			i++;
 		}
+		// Si un espace est present, l'afficher
 		if (space)
 			write(1, " ", 1);
+		Boucle pour afficher le 1er mot
 		while (start < end)
 		{
 			write(1, &av[1][start], 1);
