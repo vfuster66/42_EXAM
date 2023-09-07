6441746afce390bac6485b2967cd97e057e79803
vfuster66 <124174795+vfuster66@users.noreply.github.com>
2023-09-07 13:57:59 +0200

    Update printf.c

--- RANK_03/printf/printf.c
+++ RANK_03/printf/printf.c
@@ -1,50 +1,55 @@
 #include <unistd.h>
 #include <stdarg.h>
 
-void	put_string(char *string, int *length)
+int	put_str(char *str)
 {
-	if (!string)
-		string = "(null)";
-	while (*string)
-		*length += write(1, string++, 1);
+	int	len = 0;
+
+	if (!str)
+		str = "(null)";
+	while (*str)
+		len += write(1, str++, 1);
+	return (len);
 }
 
-void	put_digit(long long int number, int base, int *length)
+void put_digit(long long int number, int base, int *len) 
 {
-	char	*hexadecimal = "0123456789abcdef";
+	char *hexa = "0123456789abcdef";
 
 	if (number < 0)
 	{
 		number *= -1;
-		*length += write(1, "-", 1);
+		*len += write(1, "-", 1);
 	}
 	if (number >= base)
-		put_digit((number / base), base, length);
-	*length += write(1, &hexadecimal[number % base], 1);
+		put_digit(number / base, base, len);
+	*len += write(1, &hexa[number % base], 1);
 }
 
 int	ft_printf(const char *format, ...)
 {
-	int length = 0;
+	int len = 0;
 
-	va_list	pointer;
-	va_start(pointer, format);
+	va_list	ptr;
+	va_start(ptr, format);
 
 	while (*format)
 	{
-		if ((*format == '%') && ((*(format + 1) == 's') || (*(format + 1) == 'd') || (*(format + 1) == 'x')))
+		if ((*format == '%') && ((*(format + 1) == 's') 
+					|| (*(format + 1) == 'd') 
+					|| (*(format + 1) == 'x')))
 		{
 			format++;
 			if (*format == 's')
-				put_string(va_arg(pointer, char *), &length);
+				put_str(va_arg(ptr, char *), &len);
 			else if (*format == 'd')
-				put_digit((long long int)va_arg(pointer, int), 10, &length);
+				put_digit((long long int)va_arg(ptr, int), 10, &len);
 			else if (*format == 'x')
-				put_digit((long long int)va_arg(pointer, unsigned int), 16, &length);
+				put_digit((long long int)va_arg(ptr, unsigned int), 16, &len);
 		}
 		else
-			length += write(1, format, 1);
+			len += write(1, format, 1);
 		format++;
 	}
-	return (va_end(pointer), length);
+	return (va_end(ptr), len);
 }
