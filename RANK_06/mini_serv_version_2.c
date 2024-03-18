d9d91ac72a27a6927df666e9cf49042de9d31804
Virginie FUSTER PEREZ <124174795+vfuster66@users.noreply.github.com>
2024-03-18 17:28:05 +0100

    Update mini_serv_version_2.c

--- RANK_06/mini_serv_version_2.c
+++ RANK_06/mini_serv_version_2.c
@@ -9,7 +9,6 @@
 #define MAX_CLIENT 65535
 #define MESSAGE 400000
 #define MESSAGE_EXT 450000
-#define IP 2130706433
 
 void fatal()
 {
@@ -55,7 +54,7 @@ int main(int ac, char **av)
 	bzero(&servaddr, sizeof(servaddr));
 
 	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(IP);
+	servaddr.sin_addr.s_addr = htonl(2130706433);
 	servaddr.sin_port = htons(atoi(av[1]));
 
 	if ((bind(server, (const struct sockaddr *)&servaddr, sizeof(servaddr))) != 0)
@@ -76,12 +75,12 @@ int main(int ac, char **av)
 		if (select(size + 1, &old_fd, NULL, NULL, NULL) < 0)
 			fatal();
 
-		for(int connected_id = 0; connected_id <= size; connected_id++)
+		for(int connected_client = 0; connected_client <= size; connected_client++)
 		{
-			if (FD_ISSET(connected_id, &old_fd) == 0)
+			if (FD_ISSET(connected_client, &old_fd) == 0)
 				continue;
 			
-			if (connected_id == server)
+			if (connected_client == server)
 			{
 				if ((client = accept(server, NULL, NULL)) < 0)
 					fatal();
@@ -103,18 +102,18 @@ int main(int ac, char **av)
 				received_status = 1;
 
 				while (received_status == 1 && (!message[0] || message[strlen(message) - 1] != '\n'))
-					received_status = recv(connected_id, &message[strlen(message)], 1, 0);
+					received_status = recv(connected_client, &message[strlen(message)], 1, 0);
 
 				if (received_status <= 0)
 				{
-					sprintf(message_ext, "server: client %d just left\n", max_client[client]);
+					sprintf(message_ext, "server: client %d just left\n", max_client[connected_client]);
 					send_all(message_ext, server, client, size);
-					FD_CLR(connected_id, &new_fd);
-					close(connected_id);
+					FD_CLR(connected_client, &new_fd);
+					close(connected_client);
 				}
 				else
 				{
-					sprintf(message_ext, "client %d: %s", max_client[connected_id], message);
+					sprintf(message_ext, "client %d: %s", max_client[connected_client], message);
 					send_all(message_ext, server, client, size);
 				}
 			}
