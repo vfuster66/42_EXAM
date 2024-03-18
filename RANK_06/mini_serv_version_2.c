cca0e2bd88cc13dd9fc022afd428c72dada26a44
Virginie FUSTER PEREZ <124174795+vfuster66@users.noreply.github.com>
2024-03-18 09:16:45 +0100

    Update mini_serv_version_2.c

--- RANK_06/mini_serv_version_2.c
+++ RANK_06/mini_serv_version_2.c
@@ -6,42 +6,40 @@
 #include <sys/select.h>
 #include <netinet/in.h>
 
-#define MAX_CLIENTS 65535
-#define BUFFER 400000
-#define BUFFER_EXT 450000
+#define MAX_CLIENT 65535
+#define MESSAGE 450000
+#define MESSAGE_EXT 400000
 #define IP 2130706433
-#define LISTEN_QUEUE 10
 
-
-void	fatal()
+void fatal()
 {
-	write(2, "Fatal error\n", 12);
+	write (2, "Fatal error\n", 12);
 	exit(1);
 }
 
-void	send_all(char *message, int server, int client, int size)
+void send_all(char *message, int server, int client, int size)
 {
 	for (int i = 0; i <= size; i++)
 		if (i != server && i != client)
 			send(i, message, strlen(message), 0);
 }
 
-int	main(int ac, char **av)
+int main(int ac, char **av)
 {
-	int	server;
-	int	client;
-	int	size;
-	int	received_status;
-	int	max_clients[MAX_CLIENTS] = {0};
-	int	limit = 0;
+	int client;
+	int server;
+	int size;
+	int received_status;
+	int limit = 0;
+	int max_client[MAX_CLIENT];
 
 	struct sockaddr_in servaddr;
 
 	fd_set old_fd;
 	fd_set new_fd;
 
-	char buffer[BUFFER];
-	char buffer_ext[BUFFER_EXT];
+	char message[MESSAGE];
+	char message_ext[MESSAGE_EXT];
 
 	if (ac != 2)
 	{
@@ -75,49 +73,49 @@ int	main(int ac, char **av)
 	{
 		old_fd = new_fd;
 
-		if(select(size + 1, &old_fd, NULL, NULL, NULL) < 0)
+		if (select(size + 1, &old_fd, NULL, NULL, NULL) < 0)
 			fatal();
 
-		for (int connected_id = 0; connected_id <= size; connected_id++)
+		for(int connected_id = 0; connected_id <= size; connected_id++)
 		{
 			if (FD_ISSET(connected_id, &old_fd) == 0)
 				continue;
-
+			
 			if (connected_id == server)
 			{
 				if ((client = accept(server, NULL, NULL)) < 0)
 					fatal();
-
+				
 				if (client > size)
 					size = client;
 
-				max_clients[client] = limit++;
+				max_client[client] = limit++;
 
 				FD_SET(client, &new_fd);
-				sprintf(buffer, "server: client %d just arrived\n", max_clients[client]);
-				send_all(buffer, server, client, size);
+				sprintf(message, "server: client %d just arrived\n", max_client[client]);
+				send_all(message, server, client, size);
 			}
 			else
 			{
-				bzero(buffer, BUFFER);
-				bzero(buffer_ext, BUFFER_EXT);
+				bzero(message, MESSAGE);
+				bzero(message_ext, MESSAGE_EXT);
 
 				received_status = 1;
-				while (received_status == 1 && (!buffer_ext[0] || buffer_ext[strlen(buffer_ext) - 1] != '\n'))
-					received_status = recv(connected_id, &buffer_ext[strlen(buffer_ext)], 1, 0);
-				
+
+				while (received_status == 1 && (!message_ext[0] || message_ext[strlen(message_ext) - 1] != '\n'))
+					received_status = recv(connected_id, &message_ext[strlen(message_ext)], 1, 0);
+
 				if (received_status <= 0)
 				{
-					sprintf(buffer, "server: client %d just left\n", max_clients[client]);
-					send_all(buffer, server, client, size);
+					sprintf(message, "server: client %d just left\n", max_client[client]);
+					send_all(message, server, client, size);
 					FD_CLR(connected_id, &new_fd);
 					close(connected_id);
 				}
 				else
 				{
-					sprintf(buffer, "client %d: %s", max_clients[connected_id], buffer_ext);
-
-					send_all(buffer, server, client, size);
+					sprintf(message, "client %d: %s", max_client[connected_id], message_ext);
+					send_all(message, server, client, size);
 				}
 			}
 		}
